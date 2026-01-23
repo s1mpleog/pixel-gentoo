e0352e154be04cfb2d2588f20787a3dfc1441fd7
s1mpleog <mrzkk07@gmail.com>
2026-01-23 13:46:19 +0530

    launch script

---
+++ .start-hyprland.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+# Ensure XDG_RUNTIME_DIR is set
+if test -z "$XDG_RUNTIME_DIR"; then
+    export XDG_RUNTIME_DIR=$(mktemp -d /tmp/$(id -u)-runtime-dir.XXX)
+
+    if [ ! -d "$XDG_RUNTIME_DIR" ]; then
+        mkdir -p "$XDG_RUNTIME_DIR"
+        chmod 0700 "$XDG_RUNTIME_DIR"
+    fi
+fi
+
+gentoo-pipewire-launcher restart &
+exec dbus-run-session Hyprland
